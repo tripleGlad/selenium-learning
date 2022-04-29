1ad21ad751f5a49c21ff9994f37dc6be63cddf87
tripleGlad <102634078+tripleGlad@users.noreply.github.com>
2022-04-29 09:29:56 -0400

    read test data & expected result from text file

--- rolia_post/unit_test.py
+++ rolia_post/unit_test.py
@@ -10,26 +10,28 @@ class RoliaSearch(unittest.TestCase):
 
         self.driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
         self.driver.get('https://www.rolia.net/f/list.php?f=0#modal')
+        self.test_data_file = os.getcwd() + '\\test_data.txt'
         self.driver.maximize_window()
         self.actions = ActionChains(self.driver) # initialize ActionChain object
         self.current_page = None
-        self.previous_page = None
-        self.sign_in_credential_and_expected_result = None     # format like {('user_id', 'password'): True}
+        self.previous_page = None        
         self.current_page_element_dict = {}
-        self.locator_argument_fill_pair = ()
-        self.page_class = page.MainPage     
+
 
     def test_post_in_rolia_with_action_builder(self):
 
-        assert self.sign_in_credential_and_expected_result is not None              \
-            and list(self.sign_in_credential_and_expected_result.values())[0], 'Please utilize valid credential'
+        post_credential = self.exec_load_test_data(self.test_data_file, '#Post sign in credential#')
+        user_id = list(post_credential)[0][0]
+        user_password = list(post_credential)[0][1]
+        is_valid_credential = list(post_credential.values())[0]
+        if not is_valid_credential:
+            self.skipTest("invalid credential and expected result")
 
-        self.test_load_page()      
-        self.test_sign_in_in_rolia_with_action_builder()
         
-        test_data_expected_result = {(u'成功发帖，能赚💰💰💰么？', u'洗洗早点睡，梦里什么都有。😂😂🤣😂😂'):True, 		\
-			('取消按键🗙 - 测试主题', '取消按键🗙 - 测试内容'):False}
+        self.exec_load_page(page.MainPage)        
+        self.exec_sign_in_in_rolia_with_action_builder(user_id, user_password, is_valid_credential)
         
+        test_data_expected_result = self.exec_load_test_data(self.test_data_file, '#Post content and expected result#')        
         for key_value in list(test_data_expected_result.items()):            
             
             elm_click_publish_button = self.current_page.locate_element(self.current_page_element_dict.get('elm_click_publish_button'))
@@ -88,16 +90,12 @@ class RoliaSearch(unittest.TestCase):
 
             if key_value[1]:
 
-                post_author = list(self.sign_in_credential_and_expected_result.keys())[0][0]
-                               
-                self.locator_argument_fill_pair = (self.current_page_element_dict.get('elm_new_post_presentation_span'), post_author)
-                self.test_fill_locator_argument()
-                elm_new_post_presentation_span = self.current_page.locate_element(self.locator_argument_fill_pair[0])
+                updated_locator = self.exec_fill_locator_argument(self.current_page_element_dict.get('elm_new_post_presentation_span'), user_id)
+                elm_new_post_presentation_span = self.current_page.locate_element(updated_locator)
                 assert elm_new_post_presentation_span, 'Failed to submit a post.'
 
-                self.locator_argument_fill_pair = (self.current_page_element_dict.get('elm_new_post_presentation_dropdown_button'), post_author)
-                self.test_fill_locator_argument()
-                elm_new_post_presentation_dropdown_button = self.current_page.locate_element(self.locator_argument_fill_pair[0])
+                updated_locator = self.exec_fill_locator_argument(self.current_page_element_dict.get('elm_new_post_presentation_dropdown_button'), user_id)
+                elm_new_post_presentation_dropdown_button = self.current_page.locate_element(updated_locator)
 
                 self.actions.move_to_element(elm_new_post_presentation_dropdown_button)               \
                     .click(elm_new_post_presentation_dropdown_button)                                   \
@@ -106,35 +104,45 @@ class RoliaSearch(unittest.TestCase):
 
                 time.sleep(3)
 
-                self.locator_argument_fill_pair = (self.current_page_element_dict.get('elm_new_post_presentation_delete_link'), post_author)
-                self.test_fill_locator_argument()
-                elm_new_post_presentation_delete_link = self.current_page.locate_element(self.locator_argument_fill_pair[0])
+                updated_locator = self.exec_fill_locator_argument(self.current_page_element_dict.get('elm_new_post_presentation_delete_link'), user_id)
+                elm_new_post_presentation_delete_link = self.current_page.locate_element(updated_locator)
                 
                 self.actions.move_to_element(elm_new_post_presentation_delete_link)               \
                     .click(elm_new_post_presentation_delete_link)                   \
                     .pause(3)                                                       \
                     .perform()
 
-                time.sleep(2)
+                time.sleep(1)
 
                 alert = self.driver.switch_to.alert
                 alert.accept()
 
+                self.driver.switch_to.default_content()
+
             time.sleep(3)
-            saved_current_page = self.current_page
+
             
-            if self.driver.back():
-                self.current_page = self.previous_page
-                self.previous_page = saved_current_page
+        self.exec_sign_off_in_rolia_with_action_builder()
+
         
-        self.test_sign_off_in_rolia_with_action_builder()
+    def test_sign_in_and_sign_off_with_action_builder(self):
+
+        test_data_expected_result = self.exec_load_test_data(self.test_data_file, '#Sign in credential and expected result#')
+
+        self.exec_load_page(page.MainPage)
+        for key_value in list(test_data_expected_result.items()):
         
+            self.exec_sign_in_in_rolia_with_action_builder(key_value[0][0], key_value[0][1], key_value[1])
+            if key_value[1]:
+                self.exec_sign_off_in_rolia_with_action_builder()
+
+
         
     def test_search_in_rolia_with_action_builder(self):
 
-        self.test_load_page()
+        self.exec_load_page(page.MainPage)
                   
-        test_data_expected_result = {'~':False, '车险':True}
+        test_data_expected_result = self.exec_load_test_data(self.test_data_file, '#Search content and expected result#')
         for key_value in list(test_data_expected_result.items()):
 
             elm_open_search_modal_button = self.current_page.locate_element(self.current_page_element_dict.get('elm_open_search_modal_button'))
@@ -153,8 +161,7 @@ class RoliaSearch(unittest.TestCase):
                 .pause(3)                                                       \
                 .perform()
 
-            self.page_class = page.SearchResultsPage
-            self.test_load_page()
+            self.exec_load_page(page.SearchResultsPage)
             assert key_value[0] != self.current_page.locate_element(self.current_page_element_dict.get(    \
                 'elm_search_result_search_string_input')).text,       \
                 'Search result page is not shown'            
@@ -169,50 +176,47 @@ class RoliaSearch(unittest.TestCase):
                 self.previous_page = saved_current_page
 
 
-        self.page_class = page.MainPage
-        self.test_load_page()
-                     
+        self.exec_load_page(page.MainPage)
 
-    
-    def test_sign_in_in_rolia_with_action_builder(self):
-       
-        assert self.sign_in_credential_and_expected_result is not None, 'Please fill in your sign in credential and expected result.'
+                   
+
+    def exec_sign_in_in_rolia_with_action_builder(self, user_id, user_password, is_valid_credential):
+
+        assert user_id is not None and user_password is not None and is_valid_credential is not None, "miss credential and expected result"
         assert isinstance(self.current_page, page.MainPage), 'Please sign in from proper page.'
         assert self.current_page.is_sign_in_on_page(), 'Can not sign in becuase you have signed in already'
             
-        test_data_expected_result = self.sign_in_credential_and_expected_result
-        for key_value in list(test_data_expected_result.items()):
-            elm_open_sign_in_button = self.current_page.locate_element(self.current_page_element_dict.get('elm_open_sign_in_button'))
-            self.actions.move_to_element(elm_open_sign_in_button)             \
-                .click(elm_open_sign_in_button)                          \
-                .pause(3)                                                   \
-                .perform()
-           
-            elm_fill_user_id_input = self.current_page.locate_element(self.current_page_element_dict.get('elm_fill_user_id_input'))
-            elm_fill_user_password_input = self.current_page.locate_element(self.current_page_element_dict.get('elm_fill_user_password_input'))
-            elm_click_user_stay_in_checkbox = self.current_page.locate_element(self.current_page_element_dict.get('elm_click_user_stay_in_checkbox'))
-            elm_sign_on_button = self.current_page.locate_element(self.current_page_element_dict.get('elm_sign_on_button'))
-            self.actions.move_to_element(elm_fill_user_id_input)               \
-                .send_keys(key_value[0][0] + Keys.ENTER)                                            \
-                .move_to_element(elm_fill_user_password_input)               \
-                .send_keys(key_value[0][1])                                     \
-                .move_to_element(elm_click_user_stay_in_checkbox)               \
-                .click(elm_click_user_stay_in_checkbox)                         \
-                .pause(3)                                                       \
-                .move_to_element(elm_sign_on_button)                     \
-                .click(elm_sign_on_button)                               \
-                .pause(5)                                               \
-                .perform()
+        elm_open_sign_in_button = self.current_page.locate_element(self.current_page_element_dict.get('elm_open_sign_in_button'))
+        self.actions.move_to_element(elm_open_sign_in_button)             \
+            .click(elm_open_sign_in_button)                          \
+            .pause(3)                                                   \
+            .perform()
+       
+        elm_fill_user_id_input = self.current_page.locate_element(self.current_page_element_dict.get('elm_fill_user_id_input'))
+        elm_fill_user_password_input = self.current_page.locate_element(self.current_page_element_dict.get('elm_fill_user_password_input'))
+        elm_click_user_stay_in_checkbox = self.current_page.locate_element(self.current_page_element_dict.get('elm_click_user_stay_in_checkbox'))
+        elm_sign_on_button = self.current_page.locate_element(self.current_page_element_dict.get('elm_sign_on_button'))
+        self.actions.move_to_element(elm_fill_user_id_input)               \
+            .send_keys(user_id + Keys.ENTER)                                            \
+            .move_to_element(elm_fill_user_password_input)               \
+            .send_keys(user_password)                                     \
+            .move_to_element(elm_click_user_stay_in_checkbox)               \
+            .click(elm_click_user_stay_in_checkbox)                         \
+            .pause(3)                                                       \
+            .move_to_element(elm_sign_on_button)                     \
+            .click(elm_sign_on_button)                               \
+            .pause(5)                                               \
+            .perform()
 
-            time.sleep(3)
+        time.sleep(3)
 
-            assert not self.current_page.is_sign_in_on_page() or key_value[1],   \
-                f'Actual result of sign in does not match with expected result of test data = {test_data_expected_result.get(key_value[0][0])}'
+        assert self.current_page.is_sign_in_on_page() ^ is_valid_credential,   \
+            f'Actual result of sign in does not match with expected result of test data = {is_valid_credential}'
 
-         
-    def test_sign_off_in_rolia_with_action_builder(self):
-        
-        assert self.sign_in_credential_and_expected_result is not None, 'Please fill in your sign in credential and expected result.'
+
+
+    def exec_sign_off_in_rolia_with_action_builder(self):
+
         assert isinstance(self.current_page, page.MainPage), 'Please sign off from proper page.'
         assert not self.current_page.is_sign_in_on_page(), 'Sign off test can not be done because user has not signed in.'
         
@@ -228,7 +232,7 @@ class RoliaSearch(unittest.TestCase):
             .pause(3)                                                   \
             .perform()
 
-        time.sleep(3)
+        time.sleep(6)
         
         elm_go_back_button = self.current_page.locate_element(self.current_page_element_dict.get('elm_go_back_button'))            
         self.actions.move_to_element(elm_go_back_button)                     \
@@ -240,31 +244,38 @@ class RoliaSearch(unittest.TestCase):
         assert self.current_page.is_sign_in_on_page(), 'Failed to sign off!'     
 
 
-    def test_load_page(self):
+
+    def exec_load_page(self, page_class: page.BasePage):
 
         if self.current_page is not None:
             self.previous_page = self.current_page
 
-        if self.page_class == page.MainPage:
+        if page_class == page.MainPage:
             self.current_page = page.MainPage(self.driver)
             assert self.current_page.is_title_matches(), 'Main Page Is not matches'            
-        elif self.page_class == page.SearchResultsPage:
+        elif page_class == page.SearchResultsPage:
             self.current_page = page.SearchResultsPage(self.driver)
 
         self.current_page_element_dict.update(dict((t[0], t[1]) for t in self.current_page.page_element_list))
-        
-    def test_fill_locator_argument(self):
 
-        if self.locator_argument_fill_pair == ():
-            return
         
-        list_outside_tuple_value = list(self.locator_argument_fill_pair)
-        list_tuple_value = list(list_outside_tuple_value[0])
+    def exec_fill_locator_argument(self, locator, replaced_argument):
+
+        if locator == () or replaced_argument is None:
+            return locator_argument_fill_pair
+        
+        list_tuple_value = list(locator)
         list_inside_tuple_value = list(list_tuple_value[0])
-        list_inside_tuple_value[1] = list_inside_tuple_value[1].format(list_outside_tuple_value[1])
+        list_inside_tuple_value[1] = list_inside_tuple_value[1].format(replaced_argument)
         list_tuple_value[0] = tuple(list_inside_tuple_value)
-        list_outside_tuple_value[0] = tuple(list_tuple_value)
-        self.locator_argument_fill_pair = tuple(list_outside_tuple_value)
+        return tuple(list_tuple_value)
+
+    def exec_load_test_data(self, path, model):
+
+        with open(path, encoding="utf8") as f:
+            lines = [line.rstrip('\n') for line in f]
+            return ast.literal_eval(next(l.replace(model, "") for l in lines if model in l))
+
 
     def tearDown(self):
         self.driver.quit()
